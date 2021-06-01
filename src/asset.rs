3d2714161fdad05495de98603551146e3549ce28
Dr-Electron <dr-electr0n@protonmail.com>
2021-06-02 00:10:57 +0200

    Improve Filter

--- src/asset.rs
+++ src/asset.rs
@@ -1,4 +1,6 @@
+use regex::Regex;
 use serde::{Deserialize, Serialize};
+use unicode_normalization::UnicodeNormalization;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Asset {
@@ -28,7 +30,23 @@ impl Asset {
 }
 
 fn search<'a>(query: &'a str, contents: &str) -> Vec<&'a str> {
-    let _query = query.to_lowercase();
+    let mut _query = query
+        .to_lowercase()
+        //Normalize string
+        .nfd()
+        .filter(char::is_ascii)
+        .collect::<String>()
+        //Turn tabs into spaces
+        .replace("\t", " ")
+        //Get rid of zero-width spaces
+        .replace("\u{200b}", "");
+
+    //Convert multiple re-occurring whitespaces into a single space
+    let re = Regex::new(r"^[\s\p{Zs}]+|[\s\p{Zs}]+$").unwrap();
+    _query = re.replace_all(&_query, "").to_string();
+    let re = Regex::new(r"[\s\p{Zs}]{2,}").unwrap();
+    _query = re.replace_all(&_query, "").to_string();
+
     let mut results = Vec::new();
 
     for line in contents.lines() {
@@ -37,8 +55,81 @@ fn search<'a>(query: &'a str, contents: &str) -> Vec<&'a str> {
         }
         if _query.contains(&line.to_lowercase()) {
             results.push(query);
+        } else if _query.replace(" ", "").contains(&line.to_lowercase()) {
+            results.push(query);
         }
     }
 
     results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_test() {
+        let query = "tester";
+        let contents = "\
+Word1
+test
+three.";
+
+        assert_eq!(vec![query], search(query, contents));
+    }
+
+    #[test]
+    fn normalize_test() {
+        let query = "tèst";
+        let contents = "\
+Word1
+test
+three.";
+
+        assert_eq!(vec![query], search(query, contents));
+    }
+
+    #[test]
+    fn spaced_tab_test() {
+        let query = "te\tster";
+        let contents = "\
+Word1
+te st
+three.";
+
+        assert_eq!(vec![query], search(query, contents));
+    }
+
+    #[test]
+    fn zero_width_stripping_test() {
+        let query = "\u{200b}tester";
+        let contents = "\
+Word1
+test
+three.";
+
+        assert_eq!(vec![query], search(query, contents));
+    }
+
+    #[test]
+    fn multi_whitespace_stripping_test() {
+        let query = "t    e s    t  e   r";
+        let contents = "\
+Word1
+te st
+three.";
+
+        assert_eq!(vec![query], search(query, contents));
+    }
+
+    #[test]
+    fn remove_whitespaces_test() {
+        let query = "t e s t e r";
+        let contents = "\
+Word1
+test
+three.";
+
+        assert_eq!(vec![query], search(query, contents));
+    }
 }
